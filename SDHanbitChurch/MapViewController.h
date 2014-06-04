b0bd84c2cf2b7668f1347fcc4be14724859575dc
hong1211 <jaehong.chon@gmail.com>
2014-06-04 00:30:59 -0700

    add missing files to control the map

---
+++ SDHanbitChurch/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  SDHanbitChurch
+//
+//  Created by Jaehong Chon on 6/3/14.
+//  Copyright (c) 2014 San Diego Hanbit Church. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ SDHanbitChurch/MapViewController.m
@@ -0,0 +1,65 @@
+//
+//  MapViewController.m
+//  SDHanbitChurch
+//
+//  Created by Jaehong Chon on 6/3/14.
+//  Copyright (c) 2014 San Diego Hanbit Church. All rights reserved.
+//
+
+#import "MapViewController.h"
+#import "SWRevealViewController.h"
+#import <MapKit/MapKit.h>
+
+@interface MapViewController ()
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+@property (weak, nonatomic) IBOutlet MKMapView *mapControl;
+
+@end
+
+@implementation MapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Change button color
+    _sidebarButton.tintColor = [UIColor colorWithWhite:0.1f alpha:0.9f];
+    
+    // Set the side bar button action. When it's tapped, it'll show up the sidebar.
+    _sidebarButton.target = self.revealViewController;
+    _sidebarButton.action = @selector(revealToggle:);
+    
+    // Set the gesture
+    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    
+    _mapControl.showsUserLocation = YES;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
